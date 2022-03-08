004c7f514176727bfbc6ca799f03c023803a6d7a
RavellinoNP <ravelranama@gmail.com>
2022-03-08 20:54:07 +0700

    mengupload tugas 2

---
+++ lib/login_page.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String username = "";
+  String password = "";
+  bool isLoginSuccess = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Login Page Ravellino"),
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 60),
+            child: Column(children: [
+              _createImage(),
+              _usernameField(),
+              _passwordField(),
+              _loginButton(context),
+            ]),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _createImage(){
+    AssetImage assetImage = const AssetImage('assets/img/MantUnited.png');
+
+    Image image = Image(image: assetImage, height: 100, width: 100);
+    return Container(
+      child: image,
+    );
+  }
+
+  Widget _usernameField() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: TextFormField(
+        enabled: true,
+        onChanged: (value) {
+          username = value;
+        },
+        decoration: InputDecoration(
+          hintText: 'Username',
+          contentPadding: const EdgeInsets.all(8.0),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+            borderSide: BorderSide(color: Colors.blue),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+            borderSide:
+            BorderSide(color: (isLoginSuccess) ? Colors.blue : Colors.red),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _passwordField() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: TextFormField(
+        enabled: true,
+        obscureText: true,
+        onChanged: (value) {
+          password = value;
+        },
+        decoration: InputDecoration(
+          hintText: 'Password',
+          contentPadding: const EdgeInsets.all(8.0),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+            borderSide: BorderSide(color: Colors.blue),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+            borderSide:
+            BorderSide(color: (isLoginSuccess) ? Colors.blue : Colors.red),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _loginButton(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      width: MediaQuery.of(context).size.width,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: (isLoginSuccess) ? Colors.blue : Colors.red, // background
+          onPrimary: Colors.white, // foreground
+        ),
+        onPressed: () {
+          String text = "";
+          if (username == "login" && password == "12345") {
+            setState(() {
+              text = "Login Success";
+              isLoginSuccess = true;
+            });
+            _loginProcess(context);
+          } else {
+            setState(() {
+              text = "Login Failed";
+              isLoginSuccess = false;
+            });
+          }
+          print("isLoginSuccess : $isLoginSuccess");
+          SnackBar snackBar = SnackBar(
+            content: Text(text),
+          );
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        },
+        child: const Text('Login'),
+      ),
+    );
+  }
+
+  void _loginProcess(BuildContext context){}
+}
